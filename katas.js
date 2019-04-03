d32c553e3adf562ef5608a0d7a141879d87af677
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-04-03 17:39:29 +0200

    sort array of  integers

--- katas.js
+++ katas.js
@@ -112,3 +112,11 @@ console.log(BubbleSort([30, -5, 90, 4, -8]))
 selectionSort = (arr) => {
 
 }
+
+// Sort array of integers (how are they sorted)
+
+
+function isSortedAndHow(arr) {
+  return arr.every((x,i)=>i==0||arr[i]>=arr[i-1])?'yes, ascending':
+         arr.every((x,i)=>i==0||arr[i]<=arr[i-1])?'yes, descending':'no'
+}
