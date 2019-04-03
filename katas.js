15e14347505efc18e5309413639d78854fe97cde
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-04-03 17:28:26 +0200

    sort

--- katas.js
+++ katas.js
@@ -90,3 +90,25 @@ function capitalize(str) {
 }
 
 module.exports = capitalize;
+
+// bubble sort numbers
+
+BubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser
+      }
+    }
+  }
+  return arr
+}
+
+console.log(BubbleSort([30, -5, 90, 4, -8]))
+  
+
+selectionSort = (arr) => {
+
+}
